6830ff9288c78b0b76259603a4de3e9635a8958b
Damian Jasiński <dam03jas@gmail.com>
2024-11-02 17:20:13 +0100

    wszystko

---
+++ app/src/main/java/Screen/MyAppNavigation.kt
@@ -0,0 +1,126 @@
+package com.example.navhost
+
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MyAppNavigation(modifier: Modifier = Modifier,authViewModel: AuthViewModel, navController: NavHostController){
+
+
+    NavHost(navController = navController, startDestination = "login", modifier = modifier){
+        composable(route = "login"){
+            LoginScreen(modifier,navController,authViewModel)
+        }
+        composable(route = "signup"){
+            SignupScreen(modifier,navController,authViewModel)
+        }
+        composable(route = "home"){
+            HomeScreen(modifier,navController,authViewModel)
+        }
+        composable(route = "pet"){
+            PetScreen(modifier,navController,authViewModel)
+        }
+        composable(route = "settings"){
+            SettingScreen(modifier,navController,authViewModel)
+        }
+    }
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: String
+//    val hasNews: Boolean,
+//    val badgeCount: Int? = null
+)
+
+@Composable
+fun BottomNavigationTheme(navController: NavController){
+
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            route = "home"
+        ),
+        BottomNavigationItem(
+            title = "Pets",
+            selectedIcon = Icons.Filled.Add,
+            unselectedIcon = Icons.Outlined.Add,
+            route = "pet"
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings,
+            route = "settings"
+        ),
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    if(currentRoute != "login" && currentRoute != "signup"){
+        NavigationBar() {
+            items.forEach(){ item ->
+                NavigationBarItem(currentRoute == item.route,
+                    onClick = {
+                        navController.navigate(item.route)
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = item.selectedIcon,
+                            contentDescription = null,
+                        )
+                    },
+                    label = {
+                        Text(item.title)
+                    })
+            }
+        }
+    }
+}
+
+
+//fun NavController.navigateSingleTopTo(route: String) =
+//    this.navigate(route) {
+//        popUpTo(
+//            this@navigateSingleTopTo.graph.findStartDestination().id
+//        ) {
+//            saveState = true
+//        }
+//        launchSingleTop = true
+//        restoreState = true
+//    }
+//
--- app/src/main/java/com/example/vetapp/MainActivity.kt
+++ app/src/main/java/com/example/vetapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.navhost.AuthViewModel
+import com.example.navhost.BottomNavigationTheme
+import com.example.navhost.MyAppNavigation
 import com.example.vetapp.ui.theme.VetAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val authViewModel: AuthViewModel by viewModels()
         setContent {
+            val navController = rememberNavController()
             VetAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {BottomNavigationTheme(navController = navController  )}) { innerPadding ->
+                    MyAppNavigation(modifier = Modifier.padding(innerPadding),authViewModel = authViewModel, navController = navController )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VetAppTheme {
-        Greeting("Android")
-    }
-}
